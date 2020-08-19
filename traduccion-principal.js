5c022f73875735b1baa42ca23bb895e168a8221b
fedeberge <46561313+fedeberge@users.noreply.github.com>
2020-08-19 11:32:46 -0300

    Add files via upload

---
+++ traduccion-principal.js
@@ -0,0 +1,244 @@
+var linguJSON = {
+    "languages": [
+        {
+            "lang_name": "ar",
+            "lang_code": "es",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "gb",
+            "lang_code": "en",
+            "url_pattern": "?"
+        }
+
+    ],
+    "translated_segments": [
+        {
+            "source": "Quienes Somos",
+            "target_en": "Our firm"
+        },
+        {
+            "source": "Quienes somos",
+            "target_en": "Our firm"
+        },
+        {
+            "source": "Servicios",
+            "target_en_en": "Services"
+        },
+        {
+            "source": "Socios",
+            "target_en_en": "Partners"
+        },
+        {
+            "source": "Presencia Internacional",
+            "target_en": "International Presence"
+        },
+        {
+            "source": "Contacto",
+            "target_en": "Contact"
+        },
+        {
+            "source": "Auditores y Consultores Tributarios",
+            "target_en": "Accounting and Consulting Firm"
+        },
+        {
+            "source": "<b>BERGE VAZQUEZ Y ASOCIADOS</b> Es una organización de profesionales en Ciencias Económicas, con un adecuado nivel de conocimiento y experiencia práctica, que le permite resolver las situaciones a las que sus clientes resulten expuestos en el curso de sus negocios.",
+            "target_en": "<b>BERGE VAZQUEZ Y ASOCIADOS</b> is an Accounting and Consulting Firm with a suitable level of skills and expertise, that allows it to solve the situations, in which their clients are exposed in the course of  business. "
+        },
+        {
+            "source": "Sus integrantes acreditan una larga trayectoria y amplia experiencia profesional adquirida en firmas internacionales de Auditoría y Consultoría así como en empresas de primer nivel.",
+            "target_en": "Our members have a high and deep professional experience acquired at international companies of Audit and Consultancy."
+        },
+        {
+            "source": "Visión estratégica y “expertise” local son el diferencial de Berge Vazquez y Asociados, que asegura tratamiento personalizado, independencia, confidencialidad y excelencia en los servicios.",
+            "target_en": "Local strategic vision and international \"expertise \" makes the difference of Berge Vazquez y Asociados, that assures customized treatment, independence, confidentiality and excellence in the professional services."
+        },
+        {
+            "source": "Escuchamos a nuestros clientes para comprender sus necesidades.",
+            "target_en": "We listen  to our clients to understand their requirements."
+        },
+        {
+            "source": "Trabajamos en equipo con el cliente para lograr una interacción con el nivel máximo de nuestra organización.",
+            "target_en": "Our partners work as a team with the client to get an interaction with the highest level of our organization."
+        },
+        {
+            "source": "Proponemos la alternativa mas sencilla y beneficiosa para el cliente.",
+            "target_en": "We propose the easiest alternative for the client."
+        },
+        {
+            "source": "Procurando dar la mayor celeridad en la búsqueda de la solución y en la respuesta a las consultas.",
+            "target_en": "We give a quick answer to the client's questions and requirements."
+        },
+        {
+            "source": "Fácilitando acceso del cliente a los socios del Estudio",
+            "target_en": "Easy access for the client to our partners."
+        },
+        {
+            "source": "En nuestra sencilla organización todos nuestros clientes son importantes.",
+            "target_en": "And the last but not the least thing in our organization, is that all our clients are important."
+        },
+        {
+            "source": "Asesoramiento Impositivo",
+            "target_en": "Tax Advice"
+        },
+        {
+            "source": "Asesoramiento general sobre Impuestos Federales, Estaduales y Locales.",
+            "target_en": "Expect counselling on Federal, Estate and Local taxes."
+        },
+        {
+            "source": "Asesoramiento en la respuesta a requerimientos de los Organismos Recaudadores y atención de inspecciones.",
+            "target_en": "Advising in answer to requirements of the Administration and attention of tax audit. "
+        },
+        {
+            "source": "Informes sobre la incidencia impositiva en proyectos de negocio. Planeamiento impositivo.",
+            "target_en": "Reports on tax incidence in bussiness projects. Tax planning."
+        },
+        {
+            "source": "Tercerización del departamento de impuestos.",
+            "target_en": "Outsourcing of the tax department."
+        },
+        {
+            "source": "Asesoramiento personal a los directivos de la Compañía.",
+            "target_en": "Personal advising for the top management of the Company."
+        },
+        {
+            "source": "Estudios de Precios de Transferencia",
+            "target_en": "Transfer Pricing"
+        },
+        {
+            "source": "Auditoría y Servicios Contables",
+            "target_en": "Accounting and Audit"
+        },
+        {
+            "source": "Auditoría de estados contables de acuerdo con normas y prácticas profesionales nacionales e internacionales.",
+            "target_en": "Audit of Financial Statements according with local and international professional regulations."
+        },
+        {
+            "source": "Tercerización de la contabilización de operaciones.",
+            "target_en": "Outsourcing of the Accounting  Department."
+        },
+        {
+            "source": "Tercerización del departamento de auditoría interna.",
+            "target_en": "Outsourcing of the Internal Audit Department."
+        },
+        {
+            "source": "Auditorías de compra (Due Dilligence)",
+            "target_en": "Due Dilligence"
+        },
+        {
+            "source": "Servicios de Nómina",
+            "target_en": "Payroll Services"
+        },
+        {
+            "source": "Tercerización de la liquidación de haberes.",
+            "target_en": "Outsourcing of Payroll Services."
+        },
+        {
+            "source": "Liquidación de nóminas confidenciales.",
+            "target_en": "Confidential Payroll Services for the top management."
+        },
+        {
+            "source": "Asesoramiento a empresas en temas de recursos de la seguridad social.",
+            "target_en": "Advising in Social Security issues."
+        },
+        {
+            "source": "Servicio Jurídico Tributario",
+            "target_en": "Tax Legal Service"
+        },
+        {
+            "source": "Atención de Inspecciones Impositivas y de la Seguridad Social",
+            "target_en": "Attention to Tax Inspections and Social Security"
+        },
+        {
+            "source": "Presentaciones ante los Organismos Recaudadores (AFIP, Rentas)",
+            "target_en": "Presentations before Collecting Organizations (AFIP, Rentas)"
+        },
+        {
+            "source": "Patrocinio Jurídico en Recursos de Apelación ante el Tribunal Fiscal de la Nación.",
+            "target_en": "Legal Sponsorship in Appeal Resources before the Tribunal Fiscal de la Nación (National Tax Court)"
+        },
+        {
+            "source": "Patrocinio Jurídico ante la Justicia Local y Federal de temas impositivos",
+            "target_en": "Legal Sponsorship before the Local and Federal Justice regarding tax issues"
+        },
+        {
+            "source": "Socio",
+            "target_en": "Partner"
+        },
+        {
+            "source": "Contador Público (UCA).",
+            "target_en": "CPA (UCA)"
+        },
+        {
+            "source": "Abogado (UNLZ).",
+            "target_en": "Attorney (UNLZ)"
+        },
+        {
+            "source": "Especialista en Tributación (CPCECABA).",
+            "target_en": "Tax Specialist (CPCECABA)"
+        },
+        {
+            "source": "Socio de Berge Vazquez y Asociados",
+            "target_en": "Partner at Berge Vazquez y Asociados"
+        },
+
+        {
+            "source": "Contador Público (UBA)",
+            "target_en": "CPA (UBA)"
+        },
+
+        {
+            "source": "Miembro de Inpact International",
+            "target_en": "Member at Inpact International"
+        },
+        {
+            "source": "<i class=\"fa fas fa-user\"></i> Ver más",
+            "target_en": "<i class=\"fa fas fa-user\"></i> More"
+        },
+        {
+            "source": "<i class=\"fa fas fa-user\"></i>Ver más",
+            "target_en": "<i class=\"fa fas fa-user\"></i> More"
+        },
+        {
+            "source": "Presencia Internacional",
+            "target_en": "International Presence"
+        },
+        {
+            "source": "Nuestra organización es Firma Miembro en la Argentina de <b> INPACT INTERNATIONAL </b> asociación mundial de firmas de auditoría y consultoría desde el año 2001.",
+            "target_en": "Since 2001, our organization is a full member of <b>INPACT INTERNATIONAL</b> Worldwide Association of Accounting and Consulting Firms "
+        },
+        {
+            "source": "Se inicia en el año 1989 como una red de firmas europeas. Actualmente cuenta con cobertura en más de sesenta países de todo el mundo.",
+            "target_en": "INPACT INTERNATIONAL started in 1989 as an European network of firms. Nowadays INPACT INTERNATIONAL is an alliance covering more than sixty countries around the world."
+        },
+        {
+            "source": "Inpact cuenta con una sólida estructura de miembros en America Latina.",
+            "target_en": "Inpact counts with a solid structure of members in Latin America"
+        },
+
+        {
+            "source": "Dejanos tu mensaje. Te responderemos a la Brevedad.",
+            "target_en": "Leave a message. We will answer shortly"
+        },
+        {
+            "source": "Contactános",
+            "target_en": "Contact us"
+        },
+        {
+            "source": "Nombre",
+            "target_en": "Name"
+        },
+
+        {
+            "source": "Mensaje",
+            "target_en": "Message"
+        },
+        {
+            "source": "Envia",
+            "target_en": "Submit"
+        }
+
+    ],
+    "custom_lang_switcher_container_id": "menu-item-46",
+    "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\"><img src=\"[[linguTargetLang1]].svg\" height=\"15\" width=\"20\"></a></div>"
+}
